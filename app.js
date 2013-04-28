beb3d592236edf87311bbe9bd7be461f557eaf57
Jackie Jin <liveaeon@gmail.com>
2013-04-28 06:58:17 -0700

    finished implementation. no debugging yet

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+, routes = require('./routes')
+, user = require('./routes/user')
+, http = require('http')
+, path = require('path');
 //for heroku
 //var dbURL = "mongodb://localhost/codeondb";
 var dbURL = "codeondb";
@@ -42,20 +42,20 @@ var io = require('socket.io').listen(server);
 
 
 server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 /* returns true if everything is ok */ 
 function validateUsers(err, users) {
-	if (!users && !err) {
-      console.log("user doesn't exist"); 
-      return false;
-  } else if (err) {
-      console.log("db error inside socket.io " + err);
-      return false;
-  } else if (users.length !== 0 && !err) {
-      return true;
-  }
+    if (!users && !err) {
+        console.log("user doesn't exist"); 
+        return false;
+    } else if (err) {
+        console.log("db error inside socket.io " + err);
+        return false;
+    } else if (users.length !== 0 && !err) {
+        return true;
+    }
 }
 
 
@@ -69,24 +69,24 @@ io.sockets.on('connection', function (socket) {
     // requesting user data based on user key
     socket.on('userBadgeRequest', function(user_data) {
     	db.users.find({
-    		userID: user_data.userID
+    	    userID: user_data.userID
     	}, function(err, users) {
-    		if (users.length !== 0 && !err) {
-    			socket.emit('userBadgeResponse', 
-    				{userID: user_data.userID, badges:users[0].badges});
-    		}
+    	    if (users.length !== 0 && !err) {
+    		socket.emit('userBadgeResponse', 
+    			    {userID: user_data.userID, badges:users[0].badges});
+    	    }
     	})
     });
 
     // add a new badge 
     socket.on('userBadgeUpdate', function(user_data) {
     	db.user.find({userID: user_data.userID}, function(err, users) {
-    		if(validateUsers(err, users)) {
-    			users[0].badges[user_data.badgename] = db.badges.find({name: badgename});
-    			db.user.update({userID:user_data.userID}, 
-    				{$set: users[0].badges})
-    			console.log(users[0].badges);
-    		}
+    	    if(validateUsers(err, users)) {
+    		users[0].badges[user_data.badgename] = db.badges.find({name: badgename});
+    		db.user.update({userID:user_data.userID}, 
+    			       {$set: users[0].badges})
+    		console.log(users[0].badges);
+    	    }
     	})
     });
 
@@ -120,57 +120,4 @@ io.sockets.on('connection', function (socket) {
             console.log("error retrieving facebook user ID");
         }
     });
-
-
-	/*
-		BADGE LOGIC 
-	*/
-	function badgeLogics (data) {
-		this.data = data;
-
-		function giveNewBadge(badgename, userID) {
-	    socket.emit('userBadgeUpdate', {userID: user_id, badgename: badgename});
-	  }
-
-	  /* checks if badge already exists for the user */
-    function checkForBadge(badgename, badgelist) {
-      for(var badge in badgelist) {
-        if (badge.name === badgename) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-		this.cameraSweetHeart = function(photos) {
-
-		};
-
-		this.run = function() {
-			//730148408?fields=photos.fields(likes)
-			this.cameraSweetHeart(data.photos.fields);	// Over 50 likes on photo
-			/*
-			this.canIHazCheeseburger(data.location);	// Checked into restaurant
-			this.lovebirds(data.relationship);	// Got into relationship
-			this.vivaLaPapel(data.location);	//Checked into library
-			this.aRealGuitarHero(data.location);	// checked into a concert
-			this.flyOnTheWall(data.relationship); // it's complicated
-			this.gleefulPopularity(data.status); //Over 10 people comment on your status 
-			this.letThemEatCake(data.birthday); 	// birthday
-			this.lifeIsComolete(data.relationship);	// breakup
-			this.wuTanClan();	//has more than 3 sports interest 
-			this.tooManyLikes(); 	// 20+ likes on a status post
-			*/
-		};
-
-	}
 });
-
-//action definitions
-app.post('/submitFunctionName', function(req, res) {
-    db.tutorials.save({"implTitle": req.body.content}, function(err, status) {
-        console.log("function "+ req.body.content + " saved!");
-        res.redirect('/');
-    });
-});
-
--- public/javascripts/facebooklogin.js
+++ public/javascripts/facebooklogin.js
@@ -15,7 +15,7 @@ socket.on('ack', function(data) {
 
         window.fbAsyncInit = function() {
             FB.init({
-                appId      : '333483723421810', // App ID
+                appId      : '443581165736594', // App ID
                 channelUrl : './channel.html', // Channel File
                 status     : true, // check login status
                 cookie     : true, // enable cookies to allow the server to access the session
@@ -23,11 +23,120 @@ socket.on('ack', function(data) {
             });
         };
 
+        /*
+         BADGE LOGIC 
+         */
+        function badgeLogics (data) {
+            this.data = data;
+            this.user_id = data.id;
+            
+            var user_badges;
+            
+            function getUserBadges() {
+                socket.emit('userBadgeRequest', {userID: user_id});
+                socket.on('userBadgeResponse', function(user_data){
+                    user_badges = user_data.badges;
+                });
+            }
+            
+            function updateUserBadges(badgename, userID) {
+                socket.emit('userBadgeUpdate', {userID: user_id, badges: user_badges});
+            }
+            
+            /* checks if badge already exists for the user */
+            function checkForBadge(badgename, badgelist) {
+                for(var badge in badgelist) {
+                    if (badge.name === badgename) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            
+            this.vivaLaPapel = function(status_posts) {
+                if (checkForBadge())
+                    for(var i = 0; i < status_posts.length; i++) {
+                        post = status_posts[i];
+                        //console.debug(post);
+                        if(post.place && post.place.name.indexOf('Library') != -1) {  
+                            users[0].badges['Viva La Papel!'] = true;
+                        }
+                    }
+            };
+	    this.cameraSweetHeart = function(photos) {
+                var photoList = photos.data;
+                for (var photoIndex = 0; photoIndex < photoList.length; photoIndex++) {
+                    var likes = photoList[photoIndex].likes.data;
+                    if (likes.length >= 50) {
+                        //Push the badge to the photo database
+                        
+                    }
+                }                
+	    };
 
+            this.letThemEatCake = function(birthdate) {
+                var today = new Date();
+
+                var birthday = new Date(birthdate);
+
+                if (today.getMonth() == birthday.getMonth() &&
+                    today.getDate() == birthday.getDate()) {
+                    user_badges.push({
+                        name: 'Let Them Eat Cake',
+                        type: 'birthday',
+                        content: 'Happy Birthday'
+                    });
+                }
+            }
+
+	this.tooManyLikes() = function(status_posts) {
+		for(var i = 0; i < status_posts.length; i++) {
+			post = status_posts[i];
+			if (post.data["likes"] && post.data.likes.data.length >= 20) {
+				user_badges.push({
+					name: 'Too Many Likes',
+					type: 'status',
+					content: post.data.message
+				});
+			}
+		}    
+	}
+
+	// call with wuTanClan(Data.likes.data)
+	this.wuTanClan = function(likes) {
+		for(var i = 0; i < likes.length; i++) {
+			if (likes[i].category == "Sport") {
+				user_badges.push({
+					name: 'Wu Tan Clan',
+					type: 'interest', 
+					content: likes[i].name
+				});
+			}
+		}
+	}
+
+	    this.run = function() {
+	        //730148408?fields=photos.fields(likes)
+
+	         this.canIHazCheeseburger(data.location);	// Checked into restaurant
+	         this.lovebirds(data.relationship);	// Got into relationship
+	         this.vivaLaPapel(data.location);	//Checked into library
+	         this.aRealGuitarHero(data.location);	// checked into a concert
+	         this.flyOnTheWall(data.relationship); // it's complicated
+	         this.gleefulPopularity(data.status); //Over 10 people comment on your status 
+	         this.letThemEatCake(data.birthday); 	// birthday
+	         this.lifeIsComolete(data.relationship);	// breakup
+	         this.wuTanClan();	//has more than 3 sports interest 
+	         this.tooManyLikes(); 	// 20+ likes on a status post
+	         */
+	    };
+            
+        }
+        
 
         //event listeners
         document.getElementById("FacebookLogin").addEventListener("click", getLogin, false);
-      
+        
         function login() {
             FB.login(function(response) {
                 if (response.authResponse) {
@@ -48,6 +157,7 @@ socket.on('ack', function(data) {
                     // connected
                     var uid = response.authResponse.userID;
                     console.log("UserID: " + uid);
+                    
                 } else if (response.status === 'not_authorized') {
                     // not_authorized
                     $("FacebookLogin").show();
@@ -62,44 +172,11 @@ socket.on('ack', function(data) {
                 
             });
         }
-
-
-
         function testAPI() {
-          console.debug("testAPi"); 
-          FB.api('/me?fields=photos.fields(likes),statuses.limit(10)', function(response) {
-            console.debug(response)
-            /*
-            console.debug(response);
-            socket.emit('userBadgeRequest',{userID:response.id});
-            var my_badges;
-            socket.on('userBadgeResponse', function(user_badge_data){
-              if(!user_badge_data.badges) {
-                console.debug("no badges");
-                my_badges = null;
-              } else 
-                console.debug("badges: " + user_badge_data.badges);
-                my_badges = user_badge_data.badges;
+            console.debug("testAPi"); 
+            FB.api('/me?fields=photos.fields(likes),statuses.limit(10)', function(response) {
+                console.debug(response);
             });
-
-            for (i = 0; i < response.statuses.data.length; i++) {
-              status = response.statuses.data[i];
-              if (status.likes != null) {
-                console.debug(status.likes.data.length);
-                //if( !checkForBadge("Too Many Likes", my_badges) && status.likes.data.length > 10) {
-                if (true) {
-                  giveNewBadge("Too Many Likes", response.id);
-                }
-
-              break;
-              }
-
-              if (status.place != null ) {
-                console.debug(status.place.location);
-              }
-            }
-            */
-          });
         }
-      }
+    }
 });
